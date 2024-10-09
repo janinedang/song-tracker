acf92f8729138333bab13286b7d0fb9c6f187895
jdang03 <jdang03@student.ubc.ca>
2024-10-08 21:58:28 -0700

    Added tests for Song

---
+++ src/test/model/SongTest.java
@@ -0,0 +1,61 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SongTest {
+    private Song testSong;
+
+    @BeforeEach
+    void runBefore() {
+        testSong = new Song("TT", "Twice", "KPop");
+    }
+
+    // TODO
+    @Test
+    void testConstructor() {
+        assertEquals("TT", testSong.getTitle());
+        assertEquals("Twice", testSong.getArtist());
+        assertEquals("KPop", testSong.getGenre());
+        assertEquals(1, testSong.getRating());
+        assertEquals("", testSong.getReview());
+    }
+
+    // TODO
+    @Test
+    void testSetTitle() {
+        testSong.setTitle("FANCY");
+        assertEquals("FANCY", testSong.getTitle());
+    }
+
+    // TODO
+    @Test
+    void testSetArtist() {
+        testSong.setArtist("New Jeans");
+        assertEquals("New Jeans", testSong.getArtist());
+    }
+
+    // TODO
+    @Test
+    void testSetGenre() {
+        testSong.setGenre("R&B");
+        assertEquals("R&B", testSong.getGenre());
+    }
+
+    // TODO
+    @Test
+    void testSetRating() {
+        testSong.setRating(4);
+        assertEquals(4, testSong.getRating());
+    }
+
+    // TODO
+    @Test
+    void testSetReview() {
+        testSong.setReview("I love this song!");
+        assertEquals("I love this song!", testSong.getReview());
+    }
+}
