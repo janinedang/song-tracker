cd5b5d0655135103bf826df7f24ee6912a409116
jdang03 <jdang03@student.ubc.ca>
2024-10-08 21:03:43 -0700

    Added specification of Song class

--- src/main/model/Song.java
+++ src/main/model/Song.java
@@ -2,7 +2,42 @@ package model;
 
 public class Song {
 
-    public Song() {
+    // REQUIRES: length of title > 0, 1 <= rating <= 5, 0 < length of review <= 150
+    // EFFECTS: creates a song with given title, given rating, and given review
+    public Song(String title, int rating, String review) {
+        // stub
+    }
 
+    // REQUIRES: length of title > 0
+    // MODIFIES: this
+    // EFFECTS: sets this song's title to given title
+    public void setTitle(String title) {
+        // stub
+    }
+
+    // REQUIRES: 1 <= rating <= 5
+    // MODIFIES: this
+    // EFFECTS: sets this song's rating to given rating
+    public void setRating(int rating) {
+        // stub
+    }
+
+    // REQUIRES: 0 < length of review <= 150
+    // MODIFIES: this
+    // EFFECTS: sets this song's review to given review
+    public void setReview(String review) {
+        // stub
+    }
+
+    public String getTitle() {
+        return null; // stub
+    }
+
+    public int getRating() {
+        return 0; // stub
+    }
+
+    public String getReview() {
+        return null; // stub
     }
 }
