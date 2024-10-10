4250a543d94d6a83631dc8631ad2247d68678b98
jdang03 <jdang03@student.ubc.ca>
2024-10-10 00:18:58 -0700

    Created PlaylistApp class and added specification for PlaylistApp class

---
+++ src/main/ui/PlaylistApp.java
@@ -0,0 +1,75 @@
+package ui;
+
+import java.util.Scanner;
+
+import model.Song;
+
+// Playlist application
+// References the TellerApp
+public class PlaylistApp {
+
+    // EFFECTS: runs the Playlist application
+    public PlaylistApp() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runPlaylist() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes input from user
+    private void processInput(String input) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts user to input a name for the playlist
+    // and creates a new empty playlist with the given name
+    private void initPlaylist() {
+        // stub
+    }
+    // EFFECTS: displays menu of available inputs to user
+    private void displayMenu() {
+        // stub
+    }
+
+    // EFFECTS: prints current songs in playlist in order of addition
+    private void doViewSongs() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts user to add a song with a title, artist, and genre
+    private void doAddSong() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts user to choose a song to remove from playlist and removes it
+    private void doRemoveSong() {
+        // stub
+    }
+
+    // REQUIRES: user input for rating must be an integer
+    // MODIFIES: this
+    // EFFECTS: prompts user to rate the chosen song from 1-5
+    private void doRateSong() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts user to give a review of the chosen song 
+    // using 1-150 characters
+    private void doReviewSong() {
+        // stub
+    }
+
+    // EFFECTS: prompts user to give the title and artist of a song in playlist,
+    // then returns the song with the given title and artist
+    private Song selectSong() {
+        return null; // stub
+    }
+}
