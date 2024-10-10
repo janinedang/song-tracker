7c6561467dc5ba4803671fe5c468342891cf7c50
jdang03 <jdang03@student.ubc.ca>
2024-10-09 21:05:47 -0700

    Implemented Playlist Class

--- src/main/model/Playlist.java
+++ src/main/model/Playlist.java
@@ -3,65 +3,94 @@ package model;
 import java.util.ArrayList;
 
 public class Playlist {
+    private ArrayList<Song> playlist;
+    private String name;
 
     // REQUIRES: length of name > 0
     // EFFECTS: creates an empty playlist with the given name
     public Playlist(String name) {
-        // stub
+        this.playlist = new ArrayList<>();
+        this.name = name;
     }
 
-    // REQUIRES: song with given title && given artist not in playlist, 
-    //           length of title > 0, length of artist > 0, length of genre > 0
+    // REQUIRES: song with given title && given artist not in playlist,
+    // length of title > 0, length of artist > 0, length of genre > 0
     // MODIFIES: this
-    // EFFECTS: creates song with given title, given artist, and given genre and adds it to 
-    //          next empty slot in this playlist
+    // EFFECTS: creates song with given title, given artist, and given genre and
+    // adds it to
+    // next empty slot in this playlist
     public void addSong(String title, String artist, String genre) {
-        // stub
+        Song newSong = new Song(title, artist, genre);
+        playlist.add(newSong);
     }
 
     // REQUIRES: song with the given title && given artist in playlist,
-    //           length of title > 0, length of artist > 0
+    // length of title > 0, length of artist > 0
     // MODIFIES: this
     // EFFECTS: removes song with given title and given artist from this playlist
     public void removeSong(String title, String artist) {
-        // stub
+        Song scrapSong = getSong(title, artist);
+        playlist.remove(scrapSong);
     }
 
     // REQUIRES: song with the given title && given artist in playlist,
-    //           length of title > 0, length of artist > 0, 1 <= rating <= 5
+    // length of title > 0, length of artist > 0, 1 <= rating <= 5
     // MODIFIES: this
-    // EFFECTS: sets song with given title and given artist in this playlist to have the given rating
+    // EFFECTS: sets song with given title and given artist in this playlist to have
+    // the given rating
     public void rateSong(String title, String artist, int rating) {
-        // stub
+        Song songRate = getSong(title, artist);
+        songRate.setRating(rating);
     }
 
     // REQUIRES: song with the given title && given artist in playlist,
-    //           length of title > 0, length of artist > 0, 0 < length of review <= 150
+    // length of title > 0, length of artist > 0, 0 < length of review <= 150
     // MODIFIES: this
-    // EFFECTS: sets the song with the given title and given artist in this playlist to have the given review
+    // EFFECTS: sets the song with the given title and given artist in this playlist
+    // to have the given review
     public void reviewSong(String title, String artist, String review) {
-        // stub
+        Song songReview = getSong(title, artist);
+        songReview.setReview(review);
     }
 
     // REQUIRES: song with the given title && given artist in playlist,
-    //           length of title > 0, length of artist > 0
+    // length of title > 0, length of artist > 0
     // EFFECTS: returns the song with the given title and given artist
     public Song getSong(String title, String artist) {
-        return null; // stub
+        Song targetSong = null;
+
+        for (Song nextSong : playlist) {
+            if (nextSong.getTitle().equals(title) && nextSong.getArtist().equals(artist)) {
+                targetSong = nextSong;
+                break;
+            }
+        }
+
+        return targetSong;
     }
 
     // REQUIRES: length of title > 0, length of artist > 0
-    // EFFECTS: returns true if song with given title and given artist is in this playlist, 
-    //          false if song with given title and given artist is not in this playlist
+    // EFFECTS: returns true if song with given title and given artist is in this
+    // playlist,
+    // false if song with given title and given artist is not in this playlist
     public boolean inPlaylist(String title, String artist) {
-        return false; // stub
+        boolean songInPlaylist = false;
+
+        for (Song nextSong : playlist) {
+            if (nextSong.getTitle().equals(title) && nextSong.getArtist().equals(artist)) {
+                songInPlaylist = true;
+                break;
+            }
+        }
+
+        return songInPlaylist;
     }
 
     public ArrayList<Song> getPlaylist() {
-        return null; // stub
+        return playlist;
     }
 
     public String getName() {
-        return null; // stub
+        return name;
     }
 }
