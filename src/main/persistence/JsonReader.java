0f8a503eb5a31afbea07935b9104eaea666f8e2b
jdang03 <jdang03@student.ubc.ca>
2024-10-24 01:34:30 -0700

    Implemented JsonReader and updated class level documentation

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,46 +1,74 @@
 package persistence;
 
 import model.Playlist;
-import model.Song;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import org.json.*;
 
+// Represents a reader that reads workroom from JSON data stored in file
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class JsonReader {
+    private String source;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-        // stub
+        this.source = source;
     }
 
     // EFFECTS: reads playlist from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Playlist read() throws IOException {
-        return null; // stub
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parsePlaylist(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null; // stub
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses playlist from JSON object and returns it
     private Playlist parsePlaylist(JSONObject jsonObject) {
-        return null; // stub
+        String name = jsonObject.getString("name");
+        Playlist playlist = new Playlist(name);
+        addSongs(playlist, jsonObject);
+        return playlist;
     }
 
     // MODIFIES: playlist
     // EFFECTS: parses songs from JSON object and adds them to playlist
     private void addSongs(Playlist playlist, JSONObject jsonObject) {
-        // stub
+        JSONArray jsonArray = jsonObject.getJSONArray("songs");
+        for (Object json : jsonArray) {
+            JSONObject nextSong = (JSONObject) json;
+            addSong(playlist, nextSong);
+        }
     }
 
     // MODIFIES: playlist
     // EFFECTS: parses song from JSON object and adds it to playlist
     private void addSong(Playlist playlist, JSONObject jsonObject) {
-        // stub
+        String title = jsonObject.getString("title");
+        String artist = jsonObject.getString("artist");
+        String genre = jsonObject.getString("genre");
+        int rating = jsonObject.getInt("rating");
+        String review = jsonObject.getString("review");
+
+        playlist.addSong(title, artist, genre);
+        playlist.rateSong(title, artist, rating);
+        playlist.reviewSong(title, artist, review);
     }
 }
