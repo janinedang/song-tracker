a4f5575c069574d2c99af48467faffaefe702eb5
jdang03 <jdang03@student.ubc.ca>
2024-10-24 02:16:13 -0700

    Implemented JsonWriter class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -10,34 +10,38 @@ import java.io.*;
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        // stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of playlist to file
     public void write(Playlist playlist) {
-        // stub
+        JSONObject json = playlist.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
