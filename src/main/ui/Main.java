f229a2db57d3f658414623a243ad529aba8fe6be
jdang03 <jdang03@student.ubc.ca>
2024-11-21 09:48:52 -0800

    Changed Main to call PlaylistAppGUI instead of PlaylistApp

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,18 @@
 package ui;
 
-// Calls PlaylistApp class
+import javax.swing.SwingUtilities;
+
+// Calls PlaylistAppGUI class
+// Referenced from https://youtu.be/4BRUmU-ETRk?si=Y89UBQdTkuJx4Ip4
 public class Main {
     public static void main(String[] args) {
-        new PlaylistApp();
+        
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new PlaylistAppGUI();
+            }
+        });
     }
 }
