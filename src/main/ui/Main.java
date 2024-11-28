f51f0fc6a52e7e2179a6e0dff1d1512b33de43c6
jdang03 <jdang03@student.ubc.ca>
2024-11-28 06:20:24 -0800

    Added commented out call to PlaylistApp in main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,9 +6,10 @@ import javax.swing.SwingUtilities;
 // Referenced from https://youtu.be/4BRUmU-ETRk?si=Y89UBQdTkuJx4Ip4
 public class Main {
     public static void main(String[] args) {
+        // new PlaylistApp();
         
         SwingUtilities.invokeLater(new Runnable() {
-
+            
             @Override
             public void run() {
                 new PlaylistAppGUI();
